733529a944bda5a44e720cc2f7fd08483fa52b51
blryface <me@blurry.gay>
2026-02-07 22:43:27 -0300

    fix: Disable incompatible warnings.

--- src/main/java/com/moulberry/flashback/Flashback.java
+++ src/main/java/com/moulberry/flashback/Flashback.java
@@ -645,23 +645,23 @@ public class Flashback implements ModInitializer, ClientModInitializer {
 
     public static List<String> getReplayIncompatibleMods() {
         List<String> incompatible = new ArrayList<>();
-        if (FabricLoader.getInstance().isModLoaded("vmp")) {
+        /*if (FabricLoader.getInstance().isModLoaded("vmp")) {
             incompatible.add("VeryManyPlayers (vmp)");
         }
         if (FabricLoader.getInstance().isModLoaded("c2me")) {
             incompatible.add("Concurrent Chunk Management Engine (c2me)");
-        }
+        }*/
         return incompatible;
     }
 
     public static List<String> getRecordingIncompatibleMods() {
         List<String> incompatible = new ArrayList<>();
-        if (FabricLoader.getInstance().isModLoaded("farsight")) {
+        /*if (FabricLoader.getInstance().isModLoaded("farsight")) {
             incompatible.add("Farsight");
         }
         if (incompatible.isEmpty()) {
             return null;
-        }
+        }*/
         return incompatible;
     }
 
